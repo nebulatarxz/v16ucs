bbe451c9b500c75cd982d9f485a83dfc998590ec
nebula <michalciezki1130@gmail.com>
2025-03-25 17:09:16 +0100

    Update VEGA1.h

--- VEGA1.h
+++ VEGA1.h
@@ -1,27 +1,43 @@
 #ifndef VEGA1_H
 #define VEGA1_H
-
+#include <string>
 #include <SDL2/SDL.h>
-#include <iostream>
+#include <SDL2/SDL_ttf.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 const int BLOCK_SIZE = 50;
-const int SPEED = 0.001;
 
 class VEGA16 {
 public:
-    bool running;
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Rect player;
-
-    VEGA16();
+    VEGA16();  
     bool init();
     void handleEvents();
     void update();
     void render();
+    void displayMessage(const char* message, int yOffset);
+    void restartGame();
     void clean();
+    bool running;
+    void drawTree(int x, int y);
+    void checkTreeCollisions();
+    int prevPlayerX, prevPlayerY; 
+    void displayMessage(const std::string& message, int yOffset);
+
+private:
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    SDL_Rect player;
+    TTF_Font* font;
+    bool isCrushed;
+    bool isDoubleJumping;    
+    int jumpHeight;           
+    int frameCount;
+    int fps;
+    Uint32 lastTime;
+    Uint32 jumpTimer;          
 };
 
-#endif // VEGA1_H
+#endif
