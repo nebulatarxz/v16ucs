84692c1b17c61ef4a1ae41b36fe86f2dc3e7c5de
nebula <michalciezki1130@gmail.com>
2025-03-29 09:02:37 +0100

    Update and rename VEGA1.h to UCS.h

--- UCS.h
+++ UCS.h
@@ -1,16 +1,31 @@
-#ifndef VEGA1_H
-#define VEGA1_H
+#ifndef UCS_H
+#define UCS_H
 #include <string>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_image.h>
+#include <vector>
+#include <algorithm>
+
+
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int BLOCK_SIZE = 50;
+struct Bullet {
+    SDL_Rect rect;
+    bool active;
+};
+
 
 class VEGA16 {
 public:
-    VEGA16();  
+    SDL_Texture* gunTexture; 
+    std::vector<Bullet> bullets; 
+    void drawTree(int x, int y, int damage);
+
+    VEGA16();
     bool init();
     void handleEvents();
     void update();
@@ -20,24 +35,26 @@ public:
     void clean();
     bool running;
     void drawTree(int x, int y);
+	SDL_Texture* catTexture;
     void checkTreeCollisions();
-    int prevPlayerX, prevPlayerY; 
+    int prevPlayerX, prevPlayerY;
     void displayMessage(const std::string& message, int yOffset);
 
 private:
-
+    std::vector<int> treeDamage;
+    int catWidth, catHeight;
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* texture;
     SDL_Rect player;
     TTF_Font* font;
     bool isCrushed;
-    bool isDoubleJumping;    
-    int jumpHeight;           
+    bool isDoubleJumping;
+    int jumpHeight;
     int frameCount;
     int fps;
     Uint32 lastTime;
-    Uint32 jumpTimer;          
+    Uint32 jumpTimer;
 };
 
 #endif
